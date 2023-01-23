8ef5183969a24396e67f10d7bf8b1ee28d619267
newton <newtonsarker@gmail.com>
2023-01-23 06:29:38 +0100

    housed http client creation in a function

--- src/main/kotlin/ns/io/client/http/HttpClient.kt
+++ src/main/kotlin/ns/io/client/http/HttpClient.kt
@@ -13,15 +13,7 @@ import okhttp3.RequestBody.Companion.toRequestBody
 class HttpClient: IHttpClient {
 
     private val mediaType: MediaType = "application/json; charset=utf-8".toMediaType()
-    private val client = OkHttpClient.Builder()
-        .connectionPool(ConnectionPool(maxIdleConnections = 50, keepAliveDuration = 30, TimeUnit.SECONDS))
-        .dispatcher(Dispatcher().apply { maxRequests = 100 })
-        .connectTimeout(10, TimeUnit.SECONDS)
-        .writeTimeout(10, TimeUnit.SECONDS)
-        .readTimeout(30, TimeUnit.SECONDS)
-        .addInterceptor(GzipRequestInterceptor())
-        //.retryOnConnectionFailure(true).addInterceptor(RetryInterceptor(3))
-        .build()
+    private val client = createHttpClient()
 
     override fun post(url: String, jsonPayload: String): HttpResponseWrapper<String> {
         val request: Request = Request.Builder()
@@ -36,6 +28,36 @@ class HttpClient: IHttpClient {
         httpResponse.headers = response.headers.toMultimap()
         httpResponse.response = response.body?.string()
         return httpResponse
+    }
+
+    private fun createHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+
+            // Connection pooling: OkHttpClient has a connection pool that keeps track of available connections to
+            // reuse them instead of creating a new connection for each request. By default, it holds 5 connections
+            // per host.
+            .connectionPool(ConnectionPool(maxIdleConnections = 50, keepAliveDuration = 30, TimeUnit.SECONDS))
+
+            // Dispatcher: OkHttpClient uses a Dispatcher to manage the execution of requests. By default, it uses a
+            // Dispatcher that runs 64 requests in parallel.
+            .dispatcher(Dispatcher().apply { maxRequests = 100 })
+
+            // Timeouts: OkHttpClient uses timeouts to control how long to wait for a connection to be established,
+            // how long to wait for a response, and how long to wait to read data.
+            // Timeouts can be increased to handle slow or unresponsive servers.
+            .connectTimeout(10, TimeUnit.SECONDS)
+            .writeTimeout(10, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+
+            // Gzip: If the server compresses the response body using gzip, OkHttpClient can automatically
+            // decompress it. This can reduce the amount of data that needs to be sent over the network.
+            .addInterceptor(GzipRequestInterceptor())
+
+            // Retry policy: OkHttpClient can retry a request if it fails due to a non-idempotent
+            // method (like a POST request), a ConnectException, or a ProtocolException.
+            //.retryOnConnectionFailure(true).addInterceptor(RetryInterceptor(3))
+
+            .build()
     }
 
 }
